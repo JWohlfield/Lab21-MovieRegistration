dcfec1bc628d31255846b8f4356949357352815d
Jeffrey <JWohlfield@hotmail.com>
2021-02-18 17:31:21 -0500

    Initial commit

---
+++ MovieRegistration/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRegistration.Models
+{
+    public enum Genre { Action, Children, Comedy, Drama, Horror, SciFi }
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+        public int Year { get; set; }
+        public string Actors { get; set; }
+        public string Director { get; set; }
+    }
+}
